3dddc31fd7c7f4c50110b889bb4b1b78dcd17083
Dmitry Ponyatov <dponyatov@gmail.com>
2025-01-14 16:27:12 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/libc.mk
+++ mk/libc.mk
@@ -2,4 +2,6 @@ RF += $(REF)/picolibc/README.md
 $(REF)/picolibc/README.md:
 	$(GITREF) git@github.com:ponyatov/picolibc.git $(dir $@)
 
-$(REF)/newlib-salsa/README.md: /usr/src/newlib-3.3.0.tar.xz
+RF += $(REF)/newlib-salsa/README.md
+$(REF)/newlib-salsa/README.md: /usr/src/newlib/newlib-$(NEWLIB_VER).tar.xz
+	cd $(REF)/newlib-salsa ; xzcat $< | tar x
