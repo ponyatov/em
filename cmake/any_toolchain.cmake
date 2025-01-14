c468107677f03e48d2c5f7cbbd4bb690901083ec
Dmitry Ponyatov <dponyatov@gmail.com>
2025-01-14 15:25:59 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cmake/any_toolchain.cmake
+++ cmake/any_toolchain.cmake
@@ -26,6 +26,7 @@ string(TOUPPER ${OS}   OS_  )
 add_compile_options(
     "-D${HW_}" "-D${CPU_}" "-D${ARCH_}" "-D${OS_}"
     -Wall -Wextra -Wpedantic
+    $<$<CONFIG:Debug>:DEBUG>
 )
 
 if(CMAKE_BUILD_TYPE MATCHES Debug)
