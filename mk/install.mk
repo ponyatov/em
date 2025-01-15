5e3a1e3e49bf1f1d6cab3c3fbe3a8d8a2f491d9d
Dmitry Ponyatov <ponyatov@example.com>
2025-01-15 16:07:51 +0400

    Signed-off-by: Dmitry Ponyatov <ponyatov@example.com>

--- mk/install.mk
+++ mk/install.mk
@@ -1,8 +1,8 @@
 # install
 .PHONY: install update ref gz
-install: doc ref gz
-	$(MAKE) update
+install: update doc ref gz
 update:
+	sudo dpkg --add-architecture i386
 	sudo apt update
 	sudo apt install -uy `cat apt.$(shell lsb_release -si)` $(APT)
 ref: $(RF)
