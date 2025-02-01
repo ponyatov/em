2674b07e31d0762c00b7f40617e9f01d9fa2e0e6
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-01 16:39:53 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- arch/cortexM4.cmake
+++ arch/cortexM4.cmake
@@ -6,14 +6,13 @@ set(FCPU -mfpu=fpv4-sp-d16 -mfloat-abi=hard)
 add_compile_options(
     ${MCPU} ${MFPU}
 )
+
 add_compile_definitions(
     PREFETCH_ENABLE=1
     INSTRUCTION_CACHE_ENABLE=1
     DATA_CACHE_ENABLE=1
-    LSI_VALUE=40000
-    EXTERNALSAI1_CLOCK_VALUE=2097000
-    EXTERNALSAI2_CLOCK_VALUE=2097000
 )
+
 add_link_options(
     ${MCPU} ${MFPU}
 )
