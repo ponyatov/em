9e5cec65f710c250d5901d793f7072609c8bc575
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-01 20:16:27 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cross.mk
+++ mk/cross.mk
@@ -1,6 +1,8 @@
 HW ?= pc
 # HW ?= mega2560
 # HW ?= pillF030
+# HW ?= pillF103
+# HW ?= iskra
 # HW ?= f4disco
 # HW ?= l496disco
 
--- os/linux/inc/main.hpp
+++
@@ -1,6 +0,0 @@
-/// @defgroup main main
-/// @ingroup core
-/// @{
-extern int main(int argc, char *argv[]);
-extern void arg(int argc, char *argv);
-/// @}
