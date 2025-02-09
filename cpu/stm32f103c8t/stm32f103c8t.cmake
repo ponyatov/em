0a38ac455ebbfb2a304ddaf57974e2dbac5e259c
Dmitry Ponyatov <dponyatov@gmail.com>
2025-02-09 20:47:20 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpu/stm32f103c8t/stm32f103c8t.cmake
+++ cpu/stm32f103c8t/stm32f103c8t.cmake
@@ -1,6 +1,5 @@
-add_compile_options(
-)
-
 add_compile_definitions(
     STM32F103xB
 )
+
+add_compile_options()
--- cpu/stm32f407vgt/stm32f407vgt.cmake
+++ cpu/stm32f407vgt/stm32f407vgt.cmake
@@ -2,4 +2,5 @@ add_compile_definitions(
     PREFETCH_ENABLE=1
     STM32F407xx
 )
+
 add_compile_options()
