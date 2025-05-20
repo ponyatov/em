34fd37a25d5d4d3d839aa97d13e125ba0b8136b6
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-20 16:19:40 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cmake/src.cmake
+++ cmake/src.cmake
@@ -20,12 +20,12 @@ file(GLOB C
     lib/src/*.c* lib/*/src/*.c*
     # CortexM/CubeMX
     hw/${HW}/Core/Src/*.c*
-    # hw/${HW}/Drivers/CMSIS/Device/ST/${SERIES}xx/Source/*.c*
+    hw/${HW}/Drivers/CMSIS/Device/ST/${SERIES}xx/Source/*.c*
     hw/${HW}/Drivers/${SERIES}xx_HAL_Driver/Src/*.c*
-    hw/${HW}/USB_DEVICE/App/*.c* hw/${HW}/USB_DEVICE/Target/*.c*
-    hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Core/Src/*.c*
-    hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/*.c*
-    hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/*.c*
+    # hw/${HW}/USB_DEVICE/App/*.c* hw/${HW}/USB_DEVICE/Target/*.c*
+    # hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Core/Src/*.c*
+    # hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/*.c*
+    # hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Src/*.c*
 )
 
 file(GLOB H
@@ -41,12 +41,13 @@ file(GLOB H
     # CortexM/CubeMX
     hw/${HW}/Core/Inc/*.h*
     hw/${HW}/Drivers/CMSIS/Include/*.h*
-    hw/${HW}/Drivers/CMSIS/Device/ST/${SERIES}xx/Include/*.h*
+    hw/${HW}/Drivers/CMSIS/Core/Include/*.h*
     hw/${HW}/Drivers/${SERIES}xx_HAL_Driver/Inc/*.h*
-    hw/${HW}/USB_DEVICE/App/*.h* hw/${HW}/USB_DEVICE/Target/*.h*
-    hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/*.h*
-    hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/*.h*
-    hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Inc/*.h*
+    hw/${HW}/Drivers/CMSIS/Device/ST/${SERIES}xx/Include/*.h*
+    # hw/${HW}/USB_DEVICE/App/*.h* hw/${HW}/USB_DEVICE/Target/*.h*
+    # hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/*.h*
+    # hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/*.h*
+    # hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Inc/*.h*
 )
 
 file(GLOB INC
@@ -63,11 +64,12 @@ file(GLOB INC
     # CortexM/CubeMX
     hw/${HW}/Core/Inc
     hw/${HW}/Drivers/CMSIS/Include
-    hw/${HW}/Drivers/CMSIS/Device/ST/${SERIES}xx/Include
+    hw/${HW}/Drivers/CMSIS/Core/Include
     hw/${HW}/Drivers/${SERIES}xx_HAL_Driver/Inc
-    hw/${HW}/USB_DEVICE/App hw/${HW}/USB_DEVICE/Target
-    hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Core/Inc
-    hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc
-    hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Inc
+    hw/${HW}/Drivers/CMSIS/Device/ST/${SERIES}xx/Include
+    # hw/${HW}/USB_DEVICE/App hw/${HW}/USB_DEVICE/Target
+    # hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Core/Inc
+    # hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc
+    # hw/${HW}/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Inc
 )
 include_directories(${INC})
