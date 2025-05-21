7f7b01fa672801f2e5b09f380bebfe756cb53b76
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-21 22:16:22 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/merge.mk
+++ mk/merge.mk
@@ -1,7 +1,6 @@
 MERGE += Makefile README.md LICENSE
 MERGE += apt.* .clang-format .doxygen .gitignore
-MERGE += $(C) $(H) $(S)
+MERGE += $(MK) $(CM) $(C) $(H) $(F) $(P) $(R)
 MERGE += .vscode bin doc lib inc src tmp ref
 MERGE += hw cpu arch os
 MERGE += mk cmake CMake*
-MERGE += etc Dockerfile
--- mk/rule.mk
+++ mk/rule.mk
@@ -1,7 +1,7 @@
-bin/$(BINFILE): $(C) $(H) $(CP) $(HP) $(MK) $(CMK)
+bin/$(BINFILE): $(C) $(H) $(CP) $(HP) $(MK) $(CM)
 	cmake --fresh --preset linux
 	cmake --build --preset linux -j
 
-$(ELF): $(C) $(H) $(CP) $(HP) $(MK) $(CMK)
+$(ELF): $(C) $(H) $(CP) $(HP) $(MK) $(CM)
 	cmake --fresh --preset ${HW}
 	cmake --build --preset ${HW} -j
