d8c99b2d1ef9a2cccc9fb64cc3e09869eeb37974
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-26 16:24:54 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/var.mk
+++ mk/var.mk
@@ -1,10 +1,10 @@
 # var
-MODULE  = $(notdir $(CURDIR))
+APP     = $(notdir $(CURDIR))
 REL     = $(shell git rev-parse --short=4    HEAD)
 BRANCH  = $(shell git rev-parse --abbrev-ref HEAD)
 NOW     = $(shell date +%y%m%d)
 PEPS    = E26,E302,E305,E401,E402,E701,E702
-BINFILE = $(MODULE)_$(HW)_$(BRANCH)_$(REL)_$(NOW)
+BINFILE = $(APP)_$(HW)_$(BRANCH)_$(REL)_$(NOW)
 
 ifeq ($(OS),Windows_NT)
 	WS  = $(shell uname -o)
