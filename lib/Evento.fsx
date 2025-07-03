443baf8ebf10ced35404dbffff02bfe3e148219c
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-03 10:32:15 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ lib/Evento.fsx
@@ -0,0 +1,13 @@
+//! generic embedded project generation script in F#
+
+open System
+open System.IO
+
+let APP = "Evento"
+let TITLE = "Embedded Programming Language Prototype"
+let AUTHOR = "Dmitry Ponyatov"
+let EMAIL = "dponyatov@gmail.com"
+
+let LICENSE = "MIT"
+let GITHUB = $"https://github.com/ponyatov/{APP}"
+
