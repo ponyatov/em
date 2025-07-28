289a151fd22d36f55e1dea234e6133cfa43f252b
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-28 13:32:21 +0400

    .

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+//! executable file integrates VM components
+//! - parser
+//! - byte-code compiler (in-memory only)
+//! - byte-code interpreter
+
 mod config;
+mod gui;
+mod vm;
 
 use memmap2::Mmap;
 use std::fs::File;
+use std::path::Path;
 use std::io;
 use std::io::Write;
 
@@ -11,11 +19,14 @@ fn main() {
     arg(0, &argv[0]);
     for (argc, argv) in argv.iter().enumerate().skip(1) {
         arg(argc, argv);
-        let file = File::open(argv).unwrap();
-        let mmap = unsafe { Mmap::map(&file).unwrap() };
+        let file = File::open(Path::new(argv)).unwrap();
+        let src = unsafe { Mmap::map(&file).unwrap() };
+        eprintln!("File size: {} bytes", src.len());
         // eprintln!("{:?}", &mmap[..] as &str);
-        io::stdout().write_all(&mmap[..]).unwrap();
+        io::stdout().write_all(&src[..]).unwrap();
     }
+    vm::VM::new().run();
+    gui::GUI::new(&argv[0]).run();
 }
 
 fn arg(argc: usize, argv: &str) {
