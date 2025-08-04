3a22dc777df6f59f4f23e3aa0a8b2dca30eb1166
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-04 17:41:50 +0400

    .

---
+++ lib/lexer/inc/lexer.hpp
@@ -0,0 +1,9 @@
+/// @defgroup lexer lexer
+/// @ingroup compiler
+#pragma once
+
+extern int to_int(char *ts, char *te);
+extern int to_hex(char *ts, char *te);
+extern int to_oct(char *ts, char *te);
+extern int to_bin(char *ts, char *te);
+extern float to_float(char *ts, char *te);
