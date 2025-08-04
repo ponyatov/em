71d106f4e8b251ffde9526881f77e41791c4cfad
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-04 17:46:36 +0400

    .

--- lib/lexer/inc/lexer.hpp
+++ lib/lexer/inc/lexer.hpp
@@ -2,6 +2,10 @@
 /// @ingroup compiler
 #pragma once
 
+extern char *yyfile;                   //< current file name
+extern size_t yyline;                  //< current line number
+extern void lexer(char *p, char *pe);  ///< lexer
+
 extern int to_int(char *ts, char *te);
 extern int to_hex(char *ts, char *te);
 extern int to_oct(char *ts, char *te);
