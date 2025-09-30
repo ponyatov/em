8455c70fc229896bc461655f23b6301ce5b14ed5
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-30 12:02:20 +0400

    .

--- mk/rule.mk
+++ mk/rule.mk
@@ -13,3 +13,15 @@ $(CROSS)/src/%/README: $(DISTR)/%.tar.gz
 
 static/%.wasm: src/%.wat
 	wat2wasm $< -o $@ && wasm-objdump -x $@
+
+
+RU = pavel
+tmp/slide/%.ru.mp3: tmp/slide/%.ru.md mk/rule.mk
+	RHVoice-test -i $< -o $@ -p $(RU)
+
+VIDEO = -c:v libx264 -vf "scale=1280:720,fps=1" -r 1 -preset ultrafast -crf 28
+AUDIO = -c:a aac -b:a 128k -ac 1 -ar 44100
+.PHONY: video
+video: tmp/slide/slide.mp4
+# tmp/slide/slide.mp4: tmp/slide/files.audio $(MP3) $(PNG) mk/rule.mk
+# 	ffmpeg -loop 1 -i $(PNG) -f concat -i $< $(VIDEO) $(AUDIO) -shortest $@
