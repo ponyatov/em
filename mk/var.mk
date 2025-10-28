9263f8cecce50ca9fecd1df84946ce0931818ba8
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-28 15:43:50 +0400

    .

--- mk/var.mk
+++ mk/var.mk
@@ -6,6 +6,7 @@ PEPS    = E26,E302,E305,E401,E402,E701,E702
 HW     ?= pc
 BINFILE = $(APP)_$(HW)_$(BRANCH)_$(NOW)
 CORES   = $(shell grep processor /proc/cpuinfo| wc -l)
+WS      = $(shell lsb_release -si)
 
 ifeq ($(OS),Windows_NT)
 	WS  = $(shell uname -o)
