4a66e683fe7f1106eae922046930c6f3f058e8eb
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-02 10:44:19 +0400

    .

--- mk/all.mk
+++ mk/all.mk
@@ -1,9 +1,11 @@
-.PHONY: all run
+.PHONY: all run watch
 all: bin/$(BINFILE) $(S)
 	cargo build
 run: bin/$(BINFILE) $(S)
 	$^
 	cargo run -- $(S)
+watch: bin/$(BINFILE) $(S)
+	echo $^ | entr -r make run
 
 .PHONY: wasm
 wasm: static/$(APP).wasm static/hello.wasm
