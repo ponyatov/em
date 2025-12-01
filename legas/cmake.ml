7bfdcb7a4e9f9b764c0b5ba2c7b82724408456ab
Dmitry Ponyatov <dponyatov@gmail.com>
2025-12-01 15:40:45 +0400

    .

--- legas/cmake.ml
+++ legas/cmake.ml
@@ -11,7 +11,21 @@ let mkd name ?(c = "!.gitignore\n") () =
 let cMakeLists () = touch "CMakeLists.txt" ()
 let cMakePresets () = touch "CMakePresets.json" ()
 
+let install () =
+  touch "cmake/install.cmake" ~c:"set_target_properties(${CMAKE_PROJECT_NAME}
+    PROPERTIES OUTPUT_NAME ${BIN_OUTPUT_NAME}${CMAKE_EXECUTABLE_SUFFIX})
+install(TARGETS ${CMAKE_PROJECT_NAME}
+    DESTINATION ${CMAKE_INSTALL_PREFIX})
+file(CREATE_LINK ${BIN_OUTPUT_NAME}${CMAKE_EXECUTABLE_SUFFIX}
+    ${CMAKE_INSTALL_PREFIX}/${CMAKE_PROJECT_NAME} SYMBOLIC)
+" ()
+
 let cmake () =
   cMakeLists ();
   cMakePresets ();
-  mkd "cmake" ()
+  mkd "cmake" ();
+  Sys.command "cp ~/em/cmake/x86_64-linux-gnu.cmake cmake/";
+  Sys.command "cp ~/em/cmake/any_toolchain.cmake cmake/";
+  Sys.command "cp ~/em/cmake/cross.cmake cmake/";
+  Sys.command "cp ~/em/cmake/version.cmake cmake/";
+  install ()
