fb57c6bf935932b30e730f920ed21e9e9269ec47
Dmitry Ponyatov <dponyatov@gmail.com>
2025-12-17 10:57:28 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ legas/ocaml.ml
@@ -0,0 +1,61 @@
+let ocamldots () =
+  touch ".ocamlinit"
+    ~c:"#use \"topfind\";;
+#require \"unix\";;
+(* #require \"ppx_string\";; *)
+"
+    ();
+
+  let ic = Unix.open_process_in "ocamlformat --version" in
+  let version = input_line ic in
+  Unix.close_process_in ic |> ignore;
+
+  touch ".ocamlformat"
+    ~c:
+      ("version=" ^ version
+     ^ "
+profile=default
+margin=80
+line-endings=lf
+break-cases=all
+wrap-comments=true
+break-string-literals=never
+# break-infix-before-func = false
+# break-infix = fit-or-vertical
+# break-separators = after
+# let-and = sparse
+"
+      )
+    ()
+
+let dune () =
+  touch "lib/dune"
+    ~c:("(library
+  (name " ^ app ^ ")
+  (modules)
+  (libraries ppx_string))
+")
+    ();
+  let lang = "(lang dune           3.20)\n" in
+  let name = "(name                " ^ app ^ ")\n" in
+  let opam = "(generate_opam_files true)\n" in
+  let authors = "(authors             \"" ^ author ^ " <" ^ email ^ ">\")\n" in
+  let maintr = "(maintainers         \"" ^ author ^ " <" ^ email ^ ">\")\n" in
+  let bugs = "(bug_reports         \"" ^ email ^ "\")\n" in
+  let home = "(homepage            \"" ^ github ^ "\")\n" in
+  let lic = "(license             \"" ^ license ^ "\")\n" in
+  let src = "(source              (github ponyatov/" ^ app ^ "))\n" in
+  let pack = "(package\n" in
+  let syno = " (synopsis            \"" ^ title ^ "\")\n" in
+  let about = "(description         \"\")\n" in
+  let empty = "(allow_empty)\n" in
+  touch "dune-project"
+    ~c:
+      (lang ^ name ^ opam ^ authors ^ maintr ^ bugs ^ home ^ lic ^ src ^ pack
+     ^ " " ^ name ^ syno ^ about ^ empty ^ ")\n")
+    ();
+  Sys.command "dune build"
+
+let ocaml () =
+  ocamldots ();
+  dune ()
