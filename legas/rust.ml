644e56d4713dee7727cc3c1bb5f0469d8c1d7622
Dmitry Ponyatov <dponyatov@gmail.com>
2025-12-25 16:35:02 +0400

    .

--- legas/rust.ml
+++ legas/rust.ml
@@ -30,10 +30,11 @@ fn arg(argc: usize, argv: &str) {
     ()
 
 let allow = "
+#![allow(dead_code)]
+#![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(dead_code)]
+#![allow(unused_imports)]
 "
 
 let vmrs () =
