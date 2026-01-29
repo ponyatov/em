158e39d3fb85034ea370eeb39e35d712f8c1e939
Dmitry Ponyatov <dponyatov@gmail.com>
2026-01-29 16:29:11 +0400

    Merge branch 'dponyatov' of github.com:ponyatov/em into dponyatov # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- legas/python.ml
+++ legas/python.ml
@@ -1,5 +1,7 @@
 let ruff () =
-    touch ".ruff.toml" ~c:"line-length    = 80
+  touch ".ruff.toml"
+    ~c:
+      "line-length    = 80
 indent-width   = 4
 target-version = \"py310\"
 
@@ -18,4 +20,33 @@ exclude = [
 [format]
 quote-style  = \"double\"
 indent-style = \"space\"
+"
+    ()
+
+let pyvar () =
+  append "mk/var.mk" ~c:"PEPS    = E26,E302,E305,E401,E402,E701,E702
 " ()
+
+let pytool () =
+  append "mk/tool.mk"
+    ~c:
+      "#
+PY     = $(CWD)/bin/python3
+PIP    = $(CWD)/bin/pip3
+PEP    = $(CWD)/bin/autopep8 --ignore $(PEPS) -i
+"
+    ()
+
+let pymk () = touch "mk/python.mk" ~c:"$(PY) $(PIP):
+\tpython3 -m venv .
+" ()
+
+let pygit () =
+  append "lib/.gitignore" ~c:"python*/\n" ();
+  append ".gitignore" ~c:"*.pyc\nlib64\n" ()
+
+let python () =
+  pyvar ();
+  pytool ();
+  pymk ();
+  pygit ()
