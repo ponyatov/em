f6edf9169ad29e3ae0cb4e60c48b79fc263ef097
Dmitry Ponyatov <dponyatov@gmail.com>
2026-02-11 11:10:47 +0400

    .

--- legas/doc.ml
+++ legas/doc.ml
@@ -4,7 +4,7 @@ let doc () =
   Sys.command "cp ~/icons/control_64x64.png doc/logo.png"|>ignore;
   Sys.command "cp ~/em/doc/ai.md doc/"|>ignore;
   touch "doc/bib.md" ~c:"# bib\n" ();
-  mkd ("../metadoc/"^app) ();
+  mkd [%string "../metadoc/%{app}"] ();
   Sys.command [%string "cp ../metadoc/%{app}/bib.md doc/bib.md"]|>ignore;
   Sys.command [%string "cp README.md ../metadoc/%{app}/%{app}.md"]|>ignore;
   Sys.command [%string "cp doc/bib.md ../metadoc/%{app}/bib.md"]|>ignore
--- legas/mk.ml
+++ legas/mk.ml
@@ -150,15 +150,14 @@ Debian_update: apt.$(WS)
 let ai () =
   touch "mk/ai.mk"
     ~c:
-      ".PHONY: ai tmp/$(APP).ai.md
+      "\
+.PHONY: ai tmp/$(APP).ai.md
 ai: tmp/$(APP).ai.md
 tmp/$(APP).ai.md: doc
-\tcat \\
-\t\tdoc/$(APP)/*.md \\
-\t\tdoc/ai.md doc/bib.md doc/$(APP)/bib.md doc/$(APP)/*.md README.md \\
+\tcat doc/ai.md README.md doc/bib.md doc/$(APP)/bib.md \\
+\t	  doc/$(APP)/*.md \\
 \t> $@ ; touch $@
-"
-    ()
+" ()
 
 let mk () =
   mkd "mk" ();
