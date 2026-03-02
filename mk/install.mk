8c58cf6f0b5768ad9413427385d9646b75af3922
Dmitry Ponyatov <dponyatov@gmail.com>
2026-03-02 15:27:21 +0400

    .

--- mk/install.mk
+++ mk/install.mk
@@ -2,6 +2,8 @@
 # $(PIP) $(RUSTUP) $(TSC)
 install: $(WS)_install doc ref gz $(OPAM)
 	$(MAKE) update
+	sudo adduser $(USER) wireshark
+	$(MAKE) systemd
 update : $(WS)_update
 	$(RUSTUP) self update && $(RUSTUP) update
 	$(PIP) install -U    pip
@@ -13,14 +15,14 @@ gz     : $(GZ)
 
 Debian_install:
 # sudo dpkg --add-architecture i386
-Debian_update: apt.Debian
+Debian_update: apt.$(WS)
 	sudo apt update
 	sudo apt install -uy `cat $<` $(APT)
 	$(PIP) install -U    pip
 	$(PIP) install -U -r requirements.txt
 
 Ubuntu_install:
-Ubuntu_update: apt.Ubuntu
+Ubuntu_update: apt.$(WS)
 	sudo apt update
 	sudo apt install -uy `cat $<` $(APT)
 
@@ -28,3 +30,14 @@ Msys_install:
 	pacman -Suy
 Msys_update: apt.Msys
 	pacman -S $(shell cat $< | tr '\n' ' ')
+
+.PHONY: systemd
+systemd: /etc/systemd/system/$(APP)@$(USER).service
+	sudo systemctl daemon-reload
+	sudo systemctl enable $(APP)@$(USER)
+	sudo systemctl restart $(APP)@$(USER)
+	sudo systemctl status $(APP)@$(USER)
+
+/etc/systemd/system/$(APP)@$(USER).service: $(CWD)/etc/$(APP).service
+	sudo ln -fs $< $@
+	ls -la $@
