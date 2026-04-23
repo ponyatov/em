a0b400b171c51265dfbcaee80c304ab2f1632936
Dmitry Ponyatov <dponyatov@gmail.com>
2026-04-23 17:11:17 +0400

    .

--- mk/install.mk
+++ mk/install.mk
@@ -29,10 +29,10 @@ Ubuntu_update: apt.$(WS)
 Msys_install:
 # 	export  http_proxy="http://10.110.12.12:8888"
 # 	export https_proxy="http://10.110.12.12:8888"
-	pacman -S git make curl
+	pacman -S --noconfirm git make curl
 	pacman -Suy
 Msys_update: apt.Msys
-	pacman -S $(shell cat $< | tr '\n' ' ')
+	pacman -S --noconfirm $(shell cat $< | tr '\n' ' ')
 
 .PHONY: systemd
 systemd: /etc/systemd/system/$(APP)@$(USER).service
