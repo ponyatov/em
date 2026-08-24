919635b7c7f0c8d25b57de9b55b04a75c5e8274a
Dmitry Ponyatov <dponyatov@gmail.com>
2026-08-24 19:07:49 +0400

    .

--- mk/doc.mk
+++ mk/doc.mk
@@ -3,10 +3,9 @@ doc/WebAssembly-3.0-draft.pdf:
 	$(CURL) $@ https://webassembly.github.io/spec/versions/core/WebAssembly-3.0-draft.pdf
 
 .PHONY: doc
-doc: $(DOC)
-	unison $(APP)
+doc: doxy
 
 .PHONY: doxy
-doxy: .doxygen doc/DoxygenLayout.xml doc/logo.png
+doxy: .doxygen doc/DoxygenLayout.xml vscode/logo.png
 	rm -rf doc/html ; doxygen $< 1>/dev/null
 	cargo doc && cp -r target/$(RTARGET)/doc/$(APP) doc/html/
